366dbc298a7ed05b6193e3f0e19c00044fe4b8b0
jplunn23 <64892730+jplunn23@users.noreply.github.com>
2022-05-26 20:27:51 +1000

    install pip, virtualenv, ansible and activate virtualenv project notes re pip install

--- AnsiblePlone.sh
+++ AnsiblePlone.sh
@@ -1,2 +1,12 @@
 sudo apt-get update
 sudo apt-get install python
+# sudo apt-get install python-setuptools
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+sudo python get-pip.py
+sudo pip install virtualenv
+cd ~
+virtualenv xsap
+cd xsap
+bin/pip install ansible
+source bin/activate
+ansible
