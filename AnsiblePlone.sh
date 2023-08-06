49c7bb2dc12f223f518ed4593ed21e94f25cc067
jplunn23 <64892730+jplunn23@users.noreply.github.com>
2023-08-06 21:25:02 +1000

    Update AnsiblePlone.sh

--- AnsiblePlone.sh
+++ AnsiblePlone.sh
@@ -8,3 +8,7 @@ cd xsap
 bin/pip install ansible
 source bin/activate
 # ansible
+git clone https://github.com/plone/ansible-playbook.git
+cd /ansible-playbook
+ansible-galaxy install -r requirements.yml
+# now copy one of the sample*.yml files to local-configure.yml and edit as needed.
