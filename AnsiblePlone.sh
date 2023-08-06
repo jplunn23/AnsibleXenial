c76b4d0250a72844473f90848b0ecfa9ab4afee3
jplunn23 <64892730+jplunn23@users.noreply.github.com>
2023-08-06 20:52:59 +1000

    Update AnsiblePlone.sh

--- AnsiblePlone.sh
+++ AnsiblePlone.sh
@@ -1,12 +1,10 @@
 sudo apt-get update
-sudo apt-get install python
-sudo apt-get install python-setuptools
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
-sudo python get-pip.py
+# sudo python --version
+sudo apt-get install python-pip
 sudo pip install virtualenv
 cd ~
 virtualenv xsap
 cd xsap
 bin/pip install ansible
 source bin/activate
-ansible
+# ansible
